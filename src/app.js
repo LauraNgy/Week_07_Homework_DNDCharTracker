1f3cdf35ac29404c1f39f39561428a71b97802e9
Laura Nagy <ngylaura@gmail.com>
2018-09-14 15:56:31 +0100

    sets up select_view

---
+++ src/app.js
@@ -0,0 +1,7 @@
+const Races = require('./models/races.js');
+const Classes = require('./models/classes.js');
+const SelectView = require('./views/select_view.js');
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('JavaScript working');
+});
---
+++ src/views/select_view.js
@@ -0,0 +1,8 @@
+const PubSub = require('../helpers/pub_sub.js');
+const CreateAppend = require('../helpers/create_append.js');
+
+const SelectView = function () {
+
+};
+
+module.exports = SelectView;
