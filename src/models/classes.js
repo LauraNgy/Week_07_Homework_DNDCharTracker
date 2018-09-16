013824427c34ab2242980f68b7d705f719269b1f
Laura Nagy <ngylaura@gmail.com>
2018-09-16 22:45:26 +0100

    adds classes model

--- src/models/classes.js
+++ src/models/classes.js
@@ -1,7 +1,20 @@
 const PubSub = require('../helpers/pub_sub.js');
+const Request = require('../helpers/request_helper.js');
 
 const Classes = function () {
+  this.classes = null;
+};
 
+Classes.prototype.getData = function () {
+  const request = new Request('http://dnd5eapi.co/api/classes');
+  request.get()
+    .then( (data) => {
+      this.classes = data.results;
+      PubSub.publish('Classes:classes-ready', this.classes);
+    })
+    .catch( (err) => {
+      console.error(err);
+    })
 };
 
 module.exports = Classes;
